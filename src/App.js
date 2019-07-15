517281993d900a351eee6460a52a0935b5313671
Nathan Cairns <nathancairns777@gmail.com>
2019-07-15 19:21:26 +1200

    add dist to gitignore

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import LastFm from './components/LastFm';
 import './App.css';
 
 export default class App extends React.Component {
-  state = {
-    user: null,
-    userFieldValue: null,
+  constructor(props) {
+    super(props);
+    const user = window.localStorage.getItem('user');
+
+    this.state = {
+      user: user,
+      userFieldValue: null,
+    }
   }
 
   handleSubmit = (event) => {
     const { userFieldValue } = this.state;
+    window.localStorage.setItem('user', userFieldValue);
     this.setState({user: userFieldValue});
     event.preventDefault();
   }
